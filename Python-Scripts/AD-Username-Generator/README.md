6ff22d7ac6237d3cf9e89d281cd6155d65d201a4
timmccann222 <timmccann222@gmail.com>
2022-05-31 19:44:49 +0100

    AD Username Generator

---
+++ Python-Scripts/AD-Username-Generator/README.md
@@ -0,0 +1,18 @@
+# Active Directory Username Generator
+
+During a penetration test, usernames can be extracted from internal documents, social media, services (mainly web) inside the domain environments and also from the publicly available (i.e. OSINT).
+
+This python script is used to generate potential usernames based on the common AD username conventions seen below.
+
+```
+NameSurname
+Name.Surname
+NamSur
+Nam.Sur
+NSurname
+N.Surname
+SurnameName
+Surname.Name
+SurnameN
+Surname.N
+```
---
+++ Python-Scripts/AD-Username-Generator/ad_username_generator.py
@@ -0,0 +1,70 @@
+import argparse
+
+# use -f as an option which takes file/file path as an argument. 
+parser = argparse.ArgumentParser(description="Generate AD usernames based on name and surname", formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("-f", type=argparse.FileType('r'), help="Text file containing names and surnames")
+args = parser.parse_args()
+
+
+# Split List of Names and Surnames based on newline
+open_file = args.f.read().split('\n')
+
+# Contains sublist of each name and surname
+dict = []
+
+# Split each name and surname in list by blank space.
+# Add each new sublist to a new list (i.e. dict)
+for names in open_file:
+	values = names.split(' ')
+	dict.append(values)
+
+# Generate usernames based on naming coventions
+for row in dict:
+	name = row[0].lower()
+	surname = row[1].lower()
+	#NameSurname
+	print(name + surname)
+	print(name + surname)
+	#Name.Surname
+	print(name + "-" + surname)
+	#Name-Surname
+	print(name + "." + surname)
+	#NamSur
+	print(name[0:3] + surname[0:3])
+	#Nam-Sur
+	print(name[0:3] + "-" + surname[0:3])
+	#Nam.Sur
+	print(name[0:3] + "." + surname[0:3])
+	#NSurname
+	print(name[0] + surname)
+	#N-Surname
+	print(name[0] + "-" + surname)
+	#N.Surname
+	print(name[0] + "." + surname)
+	#SurnameName
+	print(surname + name)
+	#Surname-Name
+	print(surname + "-" + name)
+	#Surname.Name
+	print(surname + "." + name)
+	#SurNam
+	print(surname[0:3] + name[0:3])
+	#Sur-Nam
+	print(surname[0:3] + "-" + name[0:3])
+	#Sur.Nam
+	print(surname[0:3] + "." + name[0:3])
+	#Sname
+	print(surname[0] + name)
+	#S-name
+	print(surname[0] + "-" + name)
+	#S.name
+	print(surname[0] + "." + name)
+	#SurnameN
+	print(surname + name[0])
+	#Surname-N
+	print(surname + "-" + name[0])
+	#Surname.N
+	print(surname + "." + name[0])
+
+
+
