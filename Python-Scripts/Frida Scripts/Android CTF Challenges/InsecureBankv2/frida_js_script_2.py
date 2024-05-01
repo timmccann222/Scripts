546516891521b8d67aab0bb4e40bd89e776352a3
timmccann222 <44136817+timmccann222@users.noreply.github.com>
2024-05-01 14:01:13 +0100

    Create frida_js_script_2.py

---
+++ Python-Scripts/Frida Scripts/Android CTF Challenges/InsecureBankv2/frida_js_script_2.py
@@ -0,0 +1,26 @@
+console.log("Script loaded successfully "); // console.log is used print messages to the console
+Java.perform(function x() { 
+    console.log("Inside java perform function");
+    // Class the function belongs to.
+    var my_class = Java.use("com.android.insecurebankv2.CryptoClass");
+    // Hook the function with parameter string
+    var string_class = Java.use("java.lang.String");
+    my_class.aesDeccryptedString.overload("java.lang.String").implementation = function (x) { //hooking the function
+        console.log("*************************************")
+        //Create a new String and call the function with our new input.
+        var my_string = string_class.$new("v/sJpihDCo2ckDmLW5Uwiw==");
+        console.log("Original arg: " + x); // prints old argument to console
+        var ret = this.aesDeccryptedString(my_string); // returns result from aesDeccryptedString()
+        console.log("Return value: " + ret); // prints result to console
+        console.log("*************************************")
+        return ret;
+    };
+    //Find an instance of the class and call "aesDeccryptedString" function.
+    Java.choose("com.android.insecurebankv2", {
+        onMatch: function (instance) {
+            console.log("Found instance: " + instance);
+            console.log("Result of aesDeccryptedString func: " + instance.aesDeccryptedString());
+        },
+        onComplete: function () { }
+    });
+});
