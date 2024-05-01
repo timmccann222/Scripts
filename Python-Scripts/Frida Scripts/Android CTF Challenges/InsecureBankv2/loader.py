305d281b32a6f161a42ca82292ffd5e6fb774a0c
timmccann222 <44136817+timmccann222@users.noreply.github.com>
2024-05-01 14:02:16 +0100

    Create loader.py

---
+++ Python-Scripts/Frida Scripts/Android CTF Challenges/InsecureBankv2/loader.py
@@ -0,0 +1,14 @@
+# loader.py
+import frida
+import time
+
+device = frida.get_usb_device() # get android device
+pid = device.spawn(["com.android.insecurebankv2"]) 
+device.resume(pid)
+time.sleep(1) # Without it Java.perform silently fails
+session = device.attach(pid)
+script = session.create_script(open("frida_js_script_3.js").read())
+script.load()
+
+# Prevent the python script from terminating
+raw_input()
