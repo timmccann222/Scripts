cca062d497a6489f829e1c24938ab98090797815
timmccann222 <44136817+timmccann222@users.noreply.github.com>
2024-05-01 13:53:30 +0100

    Create loader.py

---
+++ Python-Scripts/Frida Scripts/Android CTF Challenges/Injured Android/Changing Variables/loader.py
@@ -0,0 +1,14 @@
+# loader.py
+import frida
+import time
+
+device = frida.get_usb_device() # get android device
+pid = device.spawn(["b3nac.injuredandroid"]) 
+device.resume(pid)
+time.sleep(1) # Without it Java.perform silently fails
+session = device.attach(pid)
+script = session.create_script(open("frida_js_script.js").read())
+script.load()
+
+# Prevent the python script from terminating
+raw_input()
