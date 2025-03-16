05bc1fcf4d06b7ea5c63d8fac94408941b430156
timmccann222 <44136817+timmccann222@users.noreply.github.com>
2025-03-16 11:13:24 +0000

    Create vt_report_script.py
    Created script to request report from VirusTotal based on hash value provided by user.

---
+++ Python-Scripts/Threat Research Scripts/VirusTotal Script/vt_report_script.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import configparser
+import requests
+import json
+import argparse
+from datetime import datetime
+
+# function loads configuration file.
+def load_config(file_path='config.ini'):
+    config = configparser.ConfigParser()
+    config.read(file_path)
+    return config
+
+# function retrieves API key from configuration file.
+def get_api_credentials(config):
+    try:
+        vt_api_key = config['API']['vt_api_key']
+        return vt_api_key
+    except KeyError as e:
+        print(f"Missing key in config: {e}")
+        return None
+
+# function returns VT report for hash value.
+def vt_hash_report(api_key, hash):
+    url = "https://www.virustotal.com/api/v3/files/" + hash
+    headers = {"accept": "application/json","x-apikey": api_key}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        vt_print_report(response.json())
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
+
+# function formats and displays VT report
+def vt_print_report(response):
+    if 'data' in response:
+        print(r"""
+     __     ___               _____     _        _   ____                       _
+ \ \   / (_)_ __ _   _ __|_   _|__ | |_ __ _| | |  _ \ ___ _ __   ___  _ __| |_
+  \ \ / /| | '__| | | / __|| |/ _ \| __/ _` | | | |_) / _ \ '_ \ / _ \| '__| __|
+   \ V / | | |  | |_| \__ \| | (_) | || (_| | | |  _ <  __/ |_) | (_) | |  | |_
+    \_/  |_|_|   \__,_|___/|_|\___/ \__\__,_|_| |_| \_\___| .__/ \___/|_|   \__|
+                                                          |_|
+    """)
+
+        # Color Codes
+        BOLD = '1'
+        BLUE = '34'
+        RESET = '0'
+
+        # Example of formatting for each print statement
+        print(f"\033[{BOLD};{BLUE}mVT Link:\033[{RESET}m {response['data']['links']['self']}")
+        print(f"\033[{BOLD};{BLUE}mMeaningful File Name:\033[{RESET}m {response['data']['attributes']['meaningful_name']}")
+        print(f"\033[{BOLD};{BLUE}mFile SHA256 Hash:\033[{RESET}m {response['data']['attributes']['sha256']}")
+        print(f"\033[{BOLD};{BLUE}mFile SHA1 Hash:\033[{RESET}m {response['data']['attributes']['sha1']}")
+        print(f"\033[{BOLD};{BLUE}mFile MD5 Hash:\033[{RESET}m {response['data']['attributes']['md5']}")
+        print(f"\033[{BOLD};{BLUE}mFile Reputation Score:\033[{RESET}m {response['data']['attributes']['reputation']}")
+        print(f"\033[{BOLD};{BLUE}mFile Type:\033[{RESET}m {response['data']['type']}")
+        print(f"\033[{BOLD};{BLUE}mFile Type Description:\033[{RESET}m {response['data']['attributes']['type_description']}")
+        print(f"\033[{BOLD};{BLUE}mFile Type Extension:\033[{RESET}m {response['data']['attributes']['type_extension']}")
+        print(f"\033[{BOLD};{BLUE}mCreation Date:\033[{RESET}m {datetime.fromtimestamp(response['data']['attributes']['creation_date']).strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"\033[{BOLD};{BLUE}mFirst Seen In The Wild:\033[{RESET}m {datetime.fromtimestamp(response['data']['attributes']['first_seen_itw_date']).strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"\033[{BOLD};{BLUE}mLast Modification Date:\033[{RESET}m {datetime.fromtimestamp(response['data']['attributes']['last_modification_date']).strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"\033[{BOLD};{BLUE}mFirst Submission Date:\033[{RESET}m {datetime.fromtimestamp(response['data']['attributes']['first_submission_date']).strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"\033[{BOLD};{BLUE}mLast Submission Date:\033[{RESET}m {datetime.fromtimestamp(response['data']['attributes']['last_submission_date']).strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"\033[{BOLD};{BLUE}mSuggested Threat Label:\033[{RESET}m {response['data']['attributes']['popular_threat_classification']['suggested_threat_label']}")
+    else:
+        print("\nNo file report data found.")
+
+# function defines arguments that can be passed by a user.
+def parse_args():
+    parser = argparse.ArgumentParser(description="Get file report from VirusTotal using a hash value.")
+    parser.add_argument("--hash", type=str, required=True, help="The file hash (MD5, SHA-1, or SHA-256)")
+    return parser.parse_args()
+
+# defines the main function.
+def main():
+    config = load_config()
+    vt_api_key = get_api_credentials(config)
+
+    print(f"VirusTotal API Key: {vt_api_key}")
+
+    args = parse_args()
+    hash_value = args.hash
+
+    vt_hash_report(vt_api_key, hash_value)
+
+if __name__ == "__main__":
+    main()
